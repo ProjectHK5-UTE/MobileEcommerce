7a3682f135c1333a6b7d56ee8d9c05afc219a975
Baolocace111 <baolocace111@gmail.com>
2023-04-02 22:06:26 +0700

    edit Activity

--- app/src/main/java/com/example/mobileecommerce/activity/RegisterActivity.java
+++ app/src/main/java/com/example/mobileecommerce/activity/RegisterActivity.java
@@ -19,28 +19,5 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_register);
-
-
-        EditText username = (EditText) findViewById(R.id.username);
-        EditText password = (EditText) findViewById(R.id.password);
-        EditText repassword = (EditText) findViewById(R.id.repassword);
-        MaterialButton regbtn = (MaterialButton) findViewById(R.id.signupbtn);
-
-        regbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username1 = username.getText().toString();
-                String password1 = password.getText().toString();
-                String repassword1 = repassword.getText().toString();
-                if(password1.equals(repassword1)){
-                    Toast.makeText(RegisterActivity.this,"Username is "+username1,Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(RegisterActivity.this, OTPVerifyActivity.class);
-                    startActivity(intent);
-                }
-                else
-                    Toast.makeText(RegisterActivity.this,"NOT OK!!",Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
 }
