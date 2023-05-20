c84fd8562b0cc2881b14d84190edeed226681647
sinhhung1907 <nguyensinhhung41@gmail.com>
2023-05-20 11:40:33 +0700

    a

--- app/src/main/java/com/example/mobileecommerce/retrofit/RetrofitForLogin.java
+++ app/src/main/java/com/example/mobileecommerce/retrofit/RetrofitForLogin.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitForLogin {
     private static Retrofit retrofit = null;
     static Gson gson = new GsonBuilder().setDateFormat("yyyy MM dd HH:mm:ss").create();
-    private static final String BASE_URL = "http://192.168.11.104:8088/";
+    private static final String BASE_URL = "http://192.168.56.1:8088/";
     public static Retrofit getRetrofitForLogin() {
         if(retrofit == null) {
             retrofit = new Retrofit.Builder()
